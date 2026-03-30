6c4515b88fadf350b2eba5c83523c5f5ef6e1c5f
Hasan <Hasan.che@dome.tu.ac.th>
2026-03-31 05:03:44 +0700

    modify port into EC2

--- Backend/app.py
+++ Backend/app.py
@@ -20,7 +20,7 @@ CORS(
     resources={
     r"/api/*":
     {
-        "origins": ["http://127.0.0.1:5500", "http://localhost:5500"],
+        "origins": ["http://127.0.0.1:5500", "http://localhost:5500","http://localhost:3000", "http://127.0.0.1:3000","http://35.170.64.204:3000"],
         "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         "allow_headers": ["Content-Type", "Authorization"],
         "supports_credentials": True
@@ -34,11 +34,26 @@ app.register_blueprint(assignment_bp)
 app.register_blueprint(upload_file_bp)
 app.register_blueprint(course_bp)
 
+
 @app.before_request
 def handle_preflight():
     if request.method == "OPTIONS":
+        # ดึงที่อยู่ของคนที่เรียกเข้ามา (เช่น localhost:3000 หรือ 127.0.0.1:5500)
+        origin = request.headers.get("Origin")
+        allowed_origins = [
+            "http://127.0.0.1:5500", 
+            "http://localhost:5500", 
+            "http://localhost:3000", 
+            "http://127.0.0.1:3000",
+            "http://35.170.64.204:3000"
+        ]
+        
         response = make_response()
-        response.headers.add("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+        
+        # ถ้าคนที่เรียกมา อยู่ในรายชื่อ VIP ให้ตอบกลับเป็นชื่อของคนนั้น
+        if origin in allowed_origins:
+            response.headers.add("Access-Control-Allow-Origin", origin)
+            
         response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
         response.headers.add("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
         response.headers.add("Access-Control-Allow-Credentials", "true")

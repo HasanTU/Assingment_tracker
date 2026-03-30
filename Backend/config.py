b0c8b5f8db712a890d178e65737fe62289608c43
Hasan <Hasan.che@dome.tu.ac.th>
2026-03-30 16:05:45 +0700

    rewrite migrate_to_rds.sql

--- Backend/config.py
+++ Backend/config.py
@@ -1,7 +1,21 @@
 import os
-from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
-DATABASE_URL = (f"mssql+pyodbc://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}?driver=ODBC+Driver+17+for+SQL+Server")\
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "1433")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = (
+    f"mssql+pyodbc://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    "?driver=ODBC+Driver+17+for+SQL+Server"
+)
+
+print("DATABASE_URL =", DATABASE_URL)
 
 TU_API_URL = os.getenv('TU_API_URL')
 TU_API_APPKEY = os.getenv('TU_API_APPKEY')
